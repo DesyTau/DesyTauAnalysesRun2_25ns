549a7412b239ca3183fc273d8cd3793f181c4947
fcostanz <francesco.costanza@desy.de>
2015-09-21 21:16:50 +0200

    Create recipe7412.sh

---
+++ recipe7412.sh
@@ -0,0 +1,30 @@
+export CMSSW_GIT_REFERENCE=/nfs/dust/cms/user/${USERNAME}/.cmsgit-cache
+
+cmsrel CMSSW_7_4_12
+cd CMSSW_7_4_12/src
+cmsenv
+
+git cms-init
+
+git clone https://github.com/DesyTau/DesyTauAnalysesRun2.git ${CMSSW_BASE}/src/DesyTauAnalyses
+cd ${CMSSW_BASE}/src/DesyTauAnalyses/. 
+cd ${CMSSW_BASE}/src/
+
+git clone https://github.com/veelken/SVfit_standalone.git ${CMSSW_BASE}/src/TauAnalysis/SVfitStandalone
+cd ${CMSSW_BASE}/src/TauAnalysis/SVfitStandalone
+git checkout svFit_2015Apr03
+cd ${CMSSW_BASE}/src/
+
+## Electron id
+git cms-merge-topic ikrav:egm_id_7.4.12_v1
+
+## MVA MEt
+git cms-addpkg RecoMET/METPUSubtraction/
+cd ${CMSSW_BASE}/src/RecoMET/METPUSubtraction/
+git clone https://github.com/rfriese/RecoMET-METPUSubtraction data -b 74X-13TeV-Summer15-July2015
+cd ${CMSSW_BASE}/src/
+## ## adding the pairwise MVA MEt calculation
+cp ${CMSSW_BASE}/src/DesyTauAnalyses/patch/MVAMEt/PFMETProducerMVATauTau* ${CMSSW_BASE}/src/RecoMET/METPUSubtraction/plugins/.
+
+scram b -j 32
+
