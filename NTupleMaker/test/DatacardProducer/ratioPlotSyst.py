d597afb5dd823a636684949f65b1a0e34637972c
valeria <valeria.botta@cern.ch>
2016-05-26 11:21:59 +0200

    added script to plot syst variations

---
+++ NTupleMaker/test/DatacardProducer/ratioPlotSyst.py
@@ -0,0 +1,165 @@
+######################################################################################
+# Script to plot the central, up and down distributions								 #
+# for each MC process and syst uncertainty. 										 #
+# The lower pad plots the ratios. 													 #
+# It is meant to run on ROOT files produced by the datacardProducer.py script		 #
+# 																					 #
+# Please adjust																		 #
+# - Input directory (indir)															 #	
+# - Output directory where to save plots (plotOutDir)								 #
+# - Dict with input ROOT files and variable name (varDict)							 #
+# - The name of the categories and systematics (cat, systName)						 #
+#																					 #	
+# Input argument: channel (et or mt)												 #
+# To run, e.g.:																		 #
+# python ratioPlotSyst.py mt														 #		
+# 																					 #
+######################################################################################
+
+
+
+import os, sys, ROOT
+from ROOT import *
+
+def setOutputName(dirName, catName, mcName, systName, varName):
+	return dirName+"/"+catName+"_"+mcName+"_"+systName+"_"+varName
+
+def makeRatioPlot(hcentr, hup, hdown, dirName, catName, mcName, systName, varName):
+
+	c = TCanvas("c"+varName+"_"+catName+"_"+mcName+"_"+systName, "c"+varName+"_"+catName+"_"+mcName+"_"+systName, 600,700)
+	topPad = TPad("top","top",0, 0.3, 1,1)
+	topPad.SetBottomMargin(0)
+	topPad.Draw()
+	topPad.cd()
+
+	hcentr.SetTitle(varName + " in " +catName+": "+mcName+" "+systName)
+	hcentr.Draw()
+	hup.Draw("same")
+	hdown.Draw("same")
+	hcentr.SetLineColor(kBlack)
+	hcentr.SetMarkerStyle(20)
+	hcentr.SetMarkerSize(0.7)
+	hcentr.SetMarkerColor(kBlack)
+
+	hup.SetLineColor(kBlue)
+	hup.SetMarkerStyle(22)
+	hup.SetMarkerSize(0.7)
+	hup.SetMarkerColor(kBlue)
+
+	hdown.SetLineColor(kRed)
+	hdown.SetMarkerStyle(23)
+	hdown.SetMarkerSize(0.7)
+	hdown.SetMarkerColor(kRed)
+
+	hup.SetStats(0)
+	hdown.SetStats(0)
+	hcentr.SetStats(0)
+
+	leg = TLegend(0.6, 0.7, 0.9, 0.9)
+	leg.AddEntry(hcentr, "central", "lp")
+	leg.AddEntry(hup, "syst Up","lp")
+	leg.AddEntry(hdown, "syst Down","lp") 	
+	leg.Draw()
+
+	c.cd()
+	downPad = TPad("down","down",0,0,1,0.3)
+	downPad.SetTopMargin(0)
+	downPad.Draw()
+	downPad.cd()
+
+	#making clones before dividing
+	hdown_c = hdown.Clone("")
+	hup_c  = hup.Clone("")
+	hcentr_c = hcentr.Clone("")
+	
+	hup_c.SetStats(0)
+	hdown_c.SetStats(0)
+	hcentr_c.SetStats(0)
+
+	hup_c.Divide(hcentr_c)
+	hup_c.SetAxisRange(0.,2.,"Y")
+	hup_c.Draw("e")
+	hdown_c.Divide(hcentr)
+	hdown_c.Draw("eSAME")
+	hcentr_c.Divide(hcentr)
+	hcentr_c.Draw("eSAME")
+	hup_c.SetTitle("")
+
+	c.Update()
+
+	oname = setOutputName(dirName, catName, mcName, systName, varName)
+	c.SaveAs(oname+".png")
+
+	#delete objects (maybe not needed)
+	hdown_c.Delete()
+	hup_c.Delete()
+	hcentr_c.Delete()
+
+
+
+#######################
+# beginning of "main" #
+#######################
+ROOT.TH1.SetDefaultSumw2(kTRUE)
+
+channel = sys.argv[1]
+
+# directory with input ROOT file 
+indir = "/nfs/dust/cms/user/bottav/Area76X/CMSSW_7_6_3_patch2/src/DesyTauAnalyses/NTupleMaker/test/NTuple/final/datacards/withSyst/"
+
+#specify here (varDict) the name of the input ROOT file (datacard file with systematic variations) and name of the variable (eg: "mvis", "msv", ..)
+varDict = {"mvis": "ztt_"+channel+".inputs-sm-13TeV-mvis_fall15.root"}
+
+mcProc = ["ZTT","ZL","ZJ","ZLL","TT","W","VV","QCD"]
+
+#directory for plots
+plotOutDir = "."
+
+#systematics to include and cathegories for each channel
+#Must match the names in the input ROOT file
+if channel == "et":
+	cat = ["et_inclusive", "et_inclusivemt40"]
+	systName = ["_CMS_scale_t_13TeV","_topPtWeight","_CMS_scale_eEB_13TeV", "_CMS_scale_eEE_13TeV"]
+if channel == "mt":
+	cat = ["mt_inclusivemt40", "mt_inclusivemt30"]
+	systName = ["_CMS_scale_t_13TeV","_topPtWeight"]
+else:
+	print "invalid channel choice"
+
+
+for varName, varFile in varDict.iteritems(): 	
+	inFile = TFile(indir+varFile,"read")
+	
+	#loop on file folders
+	for aCat in cat: 
+
+		print "Category ", aCat
+		#define totMC histos
+		h_totMC_centr = TH1F()
+		h_totMC_up = TH1F()
+		h_totMC_down = TH1F()
+		
+		#loop on MC processes
+		for MCname in mcProc: 
+			#take histos
+			print "Getting ", aCat+"/"+MCname
+			hCentral = inFile.Get(aCat+"/"+MCname)
+			h_totMC_centr.Add(hCentral)
+			for aSyst in systName:
+				print "Getting ", aCat+"/"+MCname+aSyst+"Down"
+				hDown = inFile.Get(aCat+"/"+MCname+aSyst+"Down")
+				h_totMC_down.Add(hDown)
+				print "Getting ", aCat+"/"+MCname+aSyst+"Up"
+				hUp = inFile.Get(aCat+"/"+MCname+aSyst+"Up")
+				h_totMC_up.Add(hUp)
+				#make ratios for each MC process, each systematic
+				makeRatioPlot(hCentral, hUp, hDown, plotOutDir, aCat, MCname, aSyst, varName)
+
+		#make ratio plot of total MC, all syst up and all down
+		makeRatioPlot(h_totMC_centr, h_totMC_up, h_totMC_down, plotOutDir, aCat, "allMC", "allSyst",varName)
+
+	inFile.Close()
+
+
+	
+
