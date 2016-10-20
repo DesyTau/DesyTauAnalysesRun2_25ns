6d777c40c0e6d25b609725ca8eb43f8963fdac44
alkaloge <alexis.kalogeropoulos@cern.ch>
2016-10-20 12:13:37 +0200

    code to make 2D templates

---
+++ NTupleMaker/test/Processing/01Jets/ElTau/wrapper_cards.sh
@@ -0,0 +1,19 @@
+
+model=$1
+ls $1*.root | awk -F "_B.root" '{print $1}' > model
+
+
+
+#for i in `ls Templ*.root`
+#for i in `ls Templates_MT2lester_met_*_${model}*.root`
+#for i in `ls Templates_met_MCTb_*_${model}*.root`
+for i in `ls Templates*_${model}*.root`
+do
+
+ln=`echo $i  | cut -d '.' -f1`
+
+echo the file is $ln
+#. cards.sh $ln stau
+. cards.sh $ln model
+
+done
---
+++ NTupleMaker/test/Processing/01Jets/MuTau/wrapper_cards.sh
@@ -0,0 +1,19 @@
+
+model=$1
+#ls $1*.root | awk -F "_B.root" '{print $1}' > model$1
+
+
+
+for i in `ls Templ*${model}*.root`
+#for i in `ls Templates_MT2lester_met_*_${model}*.root`
+#for i in `ls Templates_met_MCTb_*_${model}*.root`
+do
+
+ln=`echo $i  | cut -d '.' -f1`
+
+echo the file is $ln
+. cards.sh $ln $1
+#. cards.sh $ln C1N2
+#. cards.sh $ln C1C1
+
+done
