6874e98b0d084bdd76ef0aeb3b54109f8d745690
Mareike Meyer <mareike.meyer@desy.de>
2018-08-14 16:50:16 +0200

    updated GT

--- NTupleMaker/test/TreeProducerFromMiniAOD_94x_MC.py
+++ NTupleMaker/test/TreeProducerFromMiniAOD_94x_MC.py
@@ -25,7 +25,7 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
 
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-process.GlobalTag.globaltag = '94X_mc2017_realistic_v13'
+process.GlobalTag.globaltag = '94X_mc2017_realistic_v15'
 
 # Message Logger settings
 process.load("FWCore.MessageService.MessageLogger_cfi")
