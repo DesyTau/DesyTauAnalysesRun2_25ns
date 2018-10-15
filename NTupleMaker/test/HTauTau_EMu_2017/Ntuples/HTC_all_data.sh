0a92e4615549388943bb89fc0efd4877f8400a14
Mareike Meyer <mareike.meyer@desy.de>
2018-10-15 19:20:21 +0200

    added Filelists and submit scripts

---
+++ NTupleMaker/test/HTauTau_EMu_2017/Ntuples/HTC_all_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./HTC_submit_seq.sh SynchNTupleProducer_em_2017 analysisMacroSynch_em.conf MuonEG_Run2017B 20 
+
+./HTC_submit_seq.sh SynchNTupleProducer_em_2017 analysisMacroSynch_em.conf MuonEG_Run2017C 20
+
+./HTC_submit_seq.sh SynchNTupleProducer_em_2017 analysisMacroSynch_em.conf MuonEG_Run2017D 20
+
+./HTC_submit_seq.sh SynchNTupleProducer_em_2017 analysisMacroSynch_em.conf MuonEG_Run2017E 20 
+
+./HTC_submit_seq.sh SynchNTupleProducer_em_2017 analysisMacroSynch_em.conf MuonEG_Run2017F 20
