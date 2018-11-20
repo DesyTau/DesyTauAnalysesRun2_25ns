bc9b5b02ecad740c02c6ac32e793236f8c261f70
Mareike Meyer <mareike.meyer@desy.de>
2018-11-20 09:22:19 +0100

    less files per job

--- NTupleMaker/test/HTauTau_EMu_2017/Ntuples/HTC_all_data.sh
+++ NTupleMaker/test/HTauTau_EMu_2017/Ntuples/HTC_all_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./HTC_submit_seq.sh SynchNTupleProducer_em_2017 analysisMacroSynch_em_DATA_Run2017B.conf MuonEG_Run2017B 20 
-./HTC_submit_seq.sh SynchNTupleProducer_em_2017 analysisMacroSynch_em_DATA.conf MuonEG_Run2017CtoF 20
+./HTC_submit_seq.sh SynchNTupleProducer_em_2017 analysisMacroSynch_em_DATA_Run2017B.conf MuonEG_Run2017B 5 
+./HTC_submit_seq.sh SynchNTupleProducer_em_2017 analysisMacroSynch_em_DATA.conf MuonEG_Run2017CtoF 5
 
