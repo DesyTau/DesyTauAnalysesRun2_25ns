0565d596dba2792190b1593619288fa213e526eb
Aruna Nayak <Aruna.Nayak@cern.ch>
2018-11-21 14:33:58 +0100

    fixed small issues

--- NTupleMaker/test/TreeProducerFromMiniAOD_94x_MC.py
+++ NTupleMaker/test/TreeProducerFromMiniAOD_94x_MC.py
@@ -92,7 +92,7 @@ from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMet
 runMetCorAndUncFromMiniAOD(process,
                            isData=runOnData,
                            fixEE2017 = True,
-                           fixEE2017Params = {'userawPt': True, 'PtThreshold':50.0, 'MinEtaThreshold':2.65, 'MaxEtaThreshold': 3.139} ,
+                           fixEE2017Params = {'userawPt': True, 'ptThreshold':50.0, 'minEtaThreshold':2.65, 'maxEtaThreshold': 3.139} ,
                            postfix = "ModifiedMET"
                            )
 
