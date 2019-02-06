0b253a9ad1b68581fa15acb0dc23046dde087ae0
Merijn van de Klundert <mhfvandeklundert@gmail.com>
2019-02-06 17:51:16 +0100

    commit before pull request, cleaned up some comments

--- NTupleMaker/interface/functionsCP.h
+++ NTupleMaker/interface/functionsCP.h
@@ -45,62 +45,6 @@ double acoCP(TLorentzVector Pi1, TLorentzVector Pi2,
 	     bool firstNegative, bool pi01, bool pi02, Synch17GenTree *otree);
 
 
-/*
-void acott(const AC1B * analysisTree, Synch17Tree *otree, int tauIndex1, int tauIndex2){
-  //cout<<"tauIndex1 "<<tauIndex1<<"  tauIndex2 "<<tauIndex2<<endl;
-
-  bool correctDecay1 = analysisTree->tau_decayMode[tauIndex1]<10;
-  bool correctDecay2 = analysisTree->tau_decayMode[tauIndex2]<10;
-  bool correctDecay = correctDecay1 && correctDecay2;
-
-  if(!correctDecay){
-    otree->acotautau_00 = -9999;
-    otree->acotautau_10 = -9999;
-    otree->acotautau_01 = -9999;
-    otree->acotautau_11 = -9999;
-    return;
-  }
-  //4-momenta of charged and neutral Pi
-  TLorentzVector tau1Prong;
-  tau1Prong=chargedPivec(analysisTree,tauIndex1);
-  TLorentzVector tau2Prong;
-  tau2Prong=chargedPivec(analysisTree,tauIndex2);
-
-  bool firstNegative = false;
-  if (analysisTree->tau_charge[tauIndex1]<0.0)
-    firstNegative = true;
-
-  TLorentzVector tau1IP;
-  tau1IP = ipVec(analysisTree,tauIndex1);
-  TLorentzVector tau1Pi0;
-  tau1Pi0.SetXYZT(0.,0.,0.,0.);
-  TLorentzVector tau2IP;
-  tau2IP = ipVec(analysisTree,tauIndex2);
-  TLorentzVector tau2Pi0;
-  tau2Pi0.SetXYZT(0.,0.,0.,0.);
- 
-  if (analysisTree->tau_decayMode[tauIndex1]>=1&&analysisTree->tau_decayMode[tauIndex1]<=3) tau1Pi0 = neutralPivec(analysisTree,tauIndex1);
-  if (analysisTree->tau_decayMode[tauIndex2]>=1&&analysisTree->tau_decayMode[tauIndex2]<=3) tau2Pi0 = neutralPivec(analysisTree,tauIndex2);
-
-//  double acop = 0.; //Merijn: it is used nowhere in this function..?
-  
-  otree->acotautau_00=acoCP(tau1Prong,tau2Prong,tau1IP,tau2IP,firstNegative,false,false);
-cout<<"otree->acotautau_00 "<<otree->acotautau_00<<endl;
-
-  if (analysisTree->tau_decayMode[tauIndex1]==1&&analysisTree->tau_decayMode[tauIndex2]==0)
-    otree->acotautau_10=acoCP(tau1Prong,tau2Prong,tau1Pi0,tau2IP,firstNegative,true,false);
-
-  if (analysisTree->tau_decayMode[tauIndex1]==0&&analysisTree->tau_decayMode[tauIndex2]==1)
-    otree->acotautau_01=acoCP(tau1Prong,tau2Prong,tau1IP,tau2Pi0,firstNegative,false,true);
-
-  if (analysisTree->tau_decayMode[tauIndex1]==1&&analysisTree->tau_decayMode[tauIndex2]==1){
-    otree->acotautau_10=acoCP(tau1Prong,tau2Prong,tau1Pi0,tau2IP,firstNegative,true,false);
-    otree->acotautau_01=acoCP(tau1Prong,tau2Prong,tau1IP,tau2Pi0,firstNegative,false,true);
-    otree->acotautau_11=acoCP(tau1Prong,tau2Prong,tau1Pi0,tau2Pi0,firstNegative,true,true);
-  }
-};
-*/
-
 //Merijn: updated function to do CP calculations
 //called with: acott_Impr(&analysisTree,otree,tauIndex,leptonIndex, ch);
 //Update call instead with: acott_Impr(&analysisTree,otree,leptonIndex, tauIndex, ch). Throughout code we'll switch between mt, et and tt case!
@@ -204,19 +148,7 @@ void acott_Impr(const AC1B * analysisTree, Synch17Tree *otree, int tauIndex1, in
   }
   
 //merijn: we vetoed already if the second tau didn't contain a pion, so can safely calculate the momentum of 2nd prong from hadrons
-  
-  /*original
-  TLorentzVector tau2Prong;
-  tau2Prong=chargedPivec(analysisTree,tauIndex2);
-  int piIndexfortau2=chargedPiIndex(analysisTree,tauIndex2);
-  if(piIndexfortau2>-1){
-    otree->VxConstitTau2=analysisTree->tau_constituents_vx[tauIndex2][piIndexfortau2];
-    otree->VyConstitTau2=analysisTree->tau_constituents_vy[tauIndex2][piIndexfortau2];   
-    otree->VzConstitTau2=analysisTree->tau_constituents_vz[tauIndex2][piIndexfortau2];      
-  }
-  */
 
-  //Merijn:s
   TLorentzVector tau2Prong;
   tau2Prong=chargedPivec(analysisTree,tauIndex2);
   int piIndexfortau2=chargedPiIndex(analysisTree,tauIndex2);
@@ -225,22 +157,10 @@ void acott_Impr(const AC1B * analysisTree, Synch17Tree *otree, int tauIndex1, in
     otree->VyConstitTau2=analysisTree->tau_constituents_vy[tauIndex2][piIndexfortau2];   
     otree->VzConstitTau2=analysisTree->tau_constituents_vz[tauIndex2][piIndexfortau2];      
   }
-  
-  
-  /* obsolete calc..
-  //check if there's a muon..
-  TLorentzVector tau2Prong;
-  tau2Prong=chargedPivec(analysisTree,tauIndex2);
-  int piIndexfortau2=chargedMuIndex(analysisTree,tauIndex2);
-  if(piIndexfortau2>-1){
-    otree->VxConstitTau2=analysisTree->tau_constituents_vx[tauIndex2][piIndexfortau2];
-    otree->VyConstitTau2=analysisTree->tau_constituents_vy[tauIndex2][piIndexfortau2];   
-    otree->VzConstitTau2=analysisTree->tau_constituents_vz[tauIndex2][piIndexfortau2];      
-  }
-  */
-  
+    
   
   bool firstNegative = false;//Merijn 2019 1 10: instead I ask if the second is positive. WON'T WORK FOR E-MU CASE!
+//comment 2019 2 5: I need to re-evaluate why I implemented this. I think it is due to fact that initially lepton and muon index were innertwined
   if (analysisTree->tau_charge[tauIndex2]>0.0)
     firstNegative = true;
 
@@ -255,39 +175,15 @@ void acott_Impr(const AC1B * analysisTree, Synch17Tree *otree, int tauIndex1, in
   TLorentzVector tau2IP;
   tau2IP = ipVec(analysisTree,tauIndex2);
   TLorentzVector tau2Pi0;
-  tau2Pi0.SetXYZT(0.,0.,0.,0.);
-
-  
-  /*Merijn here want to set the angles..
-  otree->VxConstitTau1=-9999;
-  otree->VyConstitTau1=-9999;
-  otree->VzConstitTau1=-9999;
-  
-  otree->VxConstitTau2=-9999;
-  otree->VyConstitTau2=-9999;
-  otree->VzConstitTau2=-9999;
-   */
-  
+  tau2Pi0.SetXYZT(0.,0.,0.,0.); 
 
   //Merijn: here calculate neutral pion vectors. Only look for pions in first tau if channel is tt.
   if(channel=="tt"){ if(analysisTree->tau_decayMode[tauIndex1]>=1&&analysisTree->tau_decayMode[tauIndex1]<=3) tau1Pi0 = neutralPivec(analysisTree,tauIndex1);}
 
-  if (analysisTree->tau_decayMode[tauIndex2]>=1&&analysisTree->tau_decayMode[tauIndex2]<=3){ tau2Pi0 = neutralPivec(analysisTree,tauIndex2);
-    if(tau2Pi0.E()==0) cout<<"VERY STRANGE ISSUE: DECAY INDEX >0 BUT pion energy=0"<<endl;
+  if (analysisTree->tau_decayMode[tauIndex2]>=1&&analysisTree->tau_decayMode[tauIndex2]<=3){ 
+	tau2Pi0 = neutralPivec(analysisTree,tauIndex2);
   }
   
-  //here can proceed with CP calculation. Merijn: for tau index 1 it makes no sense to ask for a mode!
-  /*
-    cout<<endl;
-    // std::cout << "Mode1 = " << analysisTree->tau_decayMode[tauIndex1] << "  Mode2 = " << analysisTree->tau_decayMode[tauIndex2] << std::endl;
-    //perhaps 
-    cout<<endl;//perhaps the pdf of the constit of 1nd decaying tau. Perhaps it is pionic?
-  */
-
-  //if (analysisTree->tau_decayMode[tauIndex2]==0){
-
-  //Merijn 2019 1 18: try here to temporarily only fill dimuon case..
-  //  if()  cout<<" "<< <<endl;
 
   otree->acotautau_00=acoCP(tau1Prong,tau2Prong,tau1IP,tau2IP,firstNegative,false,false,otree);
    //I think it should work since everything assigned for 3 cases. Note: aco_00 will be filled with mu x 1-prong and mu x 1.1-prong
@@ -295,7 +191,6 @@ void acott_Impr(const AC1B * analysisTree, Synch17Tree *otree, int tauIndex1, in
   
   if(channel=="et"||channel=="mt"){//we only fill aco_01 for et and mt
     if (analysisTree->tau_decayMode[tauIndex2]==1){
-      //  cout<<"from filling 01: analysisTree->tau_decayMode[tauIndex2] "<<analysisTree->tau_decayMode[tauIndex2]<<endl;
       otree->acotautau_01=acoCP(tau1Prong,tau2Prong,tau1IP,tau2Pi0,firstNegative,false,true,otree);
     }    
   }
@@ -316,13 +211,6 @@ void acott_Impr(const AC1B * analysisTree, Synch17Tree *otree, int tauIndex1, in
     }
   }
 
-  /*
-  if(otree->acotautau_01!=-9999&&otree->tau_decay_mode_2!=1){
-    cout<<endl;
-    cout<<"Genuinely Bizar :analysisTree->tau_decayMode[tauIndex2] "<< analysisTree->tau_decayMode[tauIndex2]<<endl;
-    cout<<endl;}
-  */
-
   //  cout<<"End acott_Impr"<<endl;
 
 };
@@ -333,7 +221,7 @@ TLorentzVector chargedPivec(const AC1B * analysisTree, int tauIndex){
   piIndex=chargedPiIndex(analysisTree,tauIndex);
   TLorentzVector chargedPi;
   chargedPi.SetXYZT(0,0,0,0);
-  //cout<<"piIndex "<<piIndex <<endl;
+
   if (piIndex>-1) {
     chargedPi.SetPxPyPzE(analysisTree->tau_constituents_px[tauIndex][piIndex],
 			 analysisTree->tau_constituents_py[tauIndex][piIndex],
@@ -345,8 +233,6 @@ TLorentzVector chargedPivec(const AC1B * analysisTree, int tauIndex){
 };
 
 int chargedPiIndex(const AC1B * analysisTree, int tauIndex){
-  //  cout<<"tauIndex "<<tauIndex<<endl;
-  // cout<<"from chargedPiIndex: analysisTree->tau_decayMode[tauIndex]; "<<analysisTree->tau_decayMode[tauIndex]<<endl;
 
   int ncomponents = analysisTree->tau_constituents_count[tauIndex];
   int piIndex=-1;
@@ -467,7 +353,7 @@ TLorentzVector ipVec(const AC1B * analysisTree, int tauIndex) {
 		    analysisTree->primvertex_z);
 
     /*
-    //Merijn: temporarily add gen vertex instead..
+    //Merijn: temporarily add gen vertex instead.. please leave this code for future reference
     TVector3 vertex;
     for (unsigned int igen=0; igen<analysisTree->genparticles_count; ++igen) {
       if (analysisTree->genparticles_pdgid[igen]==23||analysisTree->genparticles_pdgid[igen]==24||
@@ -490,9 +376,6 @@ TLorentzVector ipVec(const AC1B * analysisTree, int tauIndex) {
 
     TVector3 r(0.,0.,0.);
     r=secvertex-vertex;
-   // cout<<"reco vx pion "<< analysisTree->tau_constituents_vx[tauIndex][piIndex] <<endl;
-
-    //Merijn 2019 1 13: comment the projection out; appears to me we need to project AFTER the boost.
    
     double projection=r*momenta/momenta.Mag2();
     TVector3 IP;    
@@ -565,8 +448,6 @@ momenta.SetXYZ(analysisTree->muon_px[tauIndex],
 
     TVector3 r(0.,0.,0.);
     r=secvertex-vertex;
-
-    //Merijn 2019 1 13: comment the projection out; appears to me we need to project AFTER the boost.
     
     double projection=r*momenta/momenta.Mag2();
     TVector3 IP;    
@@ -663,7 +544,6 @@ void gen_acott(const AC1B * analysisTree, Synch17GenTree *gentree, int tauIndex1
   tau2Pi0.SetXYZT(0.,0.,0.,0.);
 
   //Merijn: add to extract the charged pion info..
-  //Merijn: add to extract the charged pion info..
   gentree->VxConstitTau1=analysisTree->genparticles_vx[piIndex1];
   gentree->VyConstitTau1=analysisTree->genparticles_vy[piIndex1];
   gentree->VzConstitTau1=analysisTree->genparticles_vz[piIndex1];
@@ -671,11 +551,7 @@ void gen_acott(const AC1B * analysisTree, Synch17GenTree *gentree, int tauIndex1
   gentree->VxConstitTau2=analysisTree->genparticles_vx[piIndex2];
   gentree->VyConstitTau2=analysisTree->genparticles_vy[piIndex2];
   gentree->VzConstitTau2=analysisTree->genparticles_vz[piIndex2];
-
-  //  cout<<"gentree->VxConstitTau2 "<<gentree->VxConstitTau2<<endl;
-  if(gentree->VyConstitTau1>20) cout<<"gentree->VyConstitTau1 >20 "<<gentree->VyConstitTau1<<endl;
-  
-  // cout<<"gentree->VzConstitTau2 "<<gentree->VzConstitTau2<<endl;  
+ 
  
   if (oneProngPi01) tau1Pi0 = gen_neutralPivec(analysisTree,tauIndex1);  
   if (oneProngPi02) tau2Pi0 = gen_neutralPivec(analysisTree,tauIndex2);
@@ -856,7 +732,6 @@ TLorentzVector gen_ipVec(const AC1B * analysisTree, int tauIndex, int piIndex, T
     TVector3 r(0.,0.,0.);
     r=secvertex-vertex;
 
-    //Merijn 2019 1 13: comment the projection out; appears to me we need to project AFTER the boost.
     double projection=r*momenta/momenta.Mag2();
     TVector3 IP;    
     IP=r-momenta*projection;
@@ -1017,28 +892,22 @@ double acoCP(TLorentzVector Pi1, TLorentzVector Pi2,
   if(isinf(sign)) sign=-3;
   if(sign!=sign) sign=-3;
 
-  //Merijn: I think this is correct..
+  //Merijn: I think these assignments are correct..
   if(!pi01&&!pi02)  otree->acotautauPsi_00=sign;
-  //  cout<<"sign 00 rec"<<sign<<endl;
 
   if (pi01){ otree->acotautauPsi_10=sign;
-    //    cout<<"otree->acotautauPsi_10 "<<otree->acotautauPsi_10<<endl;
   }
   if (pi02){
     otree->acotautauPsi_01=sign;
-    // cout<<"otree->acotautauPsi_01 "<<otree->acotautauPsi_01<<endl;
   }
   if (pi01&&pi02){ otree->acotautauPsi_11=sign;
-    //   cout<<"otree->acotautauPsi_11 "<<otree->acotautauPsi_11<<endl;
   }
 
-//cout<<"acop before return from function "<<acop<<endl;
   return acop;
 }
 
 
 //Merijn: quick and dirty overload with Synch17GenTree
-//Merijn: adjust to take otree as well, conventient for debguggin..
 double acoCP(TLorentzVector Pi1, TLorentzVector Pi2, 
 	     TLorentzVector ref1, TLorentzVector ref2,
 	     bool firstNegative, bool pi01, bool pi02, Synch17GenTree *otree) {
@@ -1106,21 +975,12 @@ double acoCP(TLorentzVector Pi1, TLorentzVector Pi2,
   if(isinf(sign)) sign=-3;
   if(sign!=sign) sign=-3;
       
-  //Merijn: I think this is correct..
+  //Merijn: I think this is correct assignment
   //Merijn: I think this is correct..
   if(!pi01&&!pi02)  otree->acotautauPsi_00=sign;
-  //  cout<<"sign 00 gen"<<sign<<endl;
-  if (pi01){ otree->acotautauPsi_10=sign;
-    // cout<<"gen: otree->acotautauPsi_10 "<<otree->acotautauPsi_10<<endl;
-  }
-  if (pi02){
-    otree->acotautauPsi_01=sign;
-    // cout<<"gen: otree->acotautauPsi_01 "<<otree->acotautauPsi_01<<endl;
-  }
-  if (pi01&&pi02){ otree->acotautauPsi_11=sign;
-    // cout<<"gen: otree->acotautauPsi_11 "<<otree->acotautauPsi_11<<endl;
-  }
+  if (pi01){ otree->acotautauPsi_10=sign; }
+  if (pi02){ otree->acotautauPsi_01=sign;}
+  if (pi01&&pi02){ otree->acotautauPsi_11=sign;}
 
-//cout<<"acop before return from function "<<acop<<endl;
   return acop;
 }
