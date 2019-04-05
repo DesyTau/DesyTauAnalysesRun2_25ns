7d5bb1a63caf4dafbd134ed1921f088c58f5dca0
Diwakar01 <43076488+Diwakar01@users.noreply.github.com>
2019-04-05 14:51:05 +0530

    Update Synch17Tree.cc

--- NTupleMaker/src/Synch17Tree.cc
+++ NTupleMaker/src/Synch17Tree.cc
@@ -342,6 +342,12 @@ void Synch17Tree::ReadInit(TTree *tree)
    fChain->SetBranchAddress("GenVertexX", &GenVertexX, &b_GenVertexX);
    fChain->SetBranchAddress("GenVertexY", &GenVertexY, &b_GenVertexY);
    fChain->SetBranchAddress("GenVertexZ", &GenVertexZ, &b_GenVertexZ);
+	
+   //refitvertex info
+   fChain->SetBranchAddress("RefitVertexX", &RefitVertexX, &b_RefitVertexX);
+   fChain->SetBranchAddress("RefitVertexY", &RefitVertexY, &b_RefitVertexY);
+   fChain->SetBranchAddress("RefitVertexZ", &RefitVertexZ, &b_RefitVertexZ);
+   fChain->SetBranchAddress("matched_pair", &matched_pair, &b_matched_pair);
    
    fChain->SetBranchAddress("VxConstitTau1", &VxConstitTau1, &b_VxConstitTau1);
    fChain->SetBranchAddress("VyConstitTau1", &VyConstitTau1, &b_VyConstitTau1);
@@ -707,6 +713,12 @@ void Synch17Tree::WriteInit(TTree *tree) {
    fChain->Branch("GenVertexX", &GenVertexX, "GenVertexX/F");
    fChain->Branch("GenVertexY", &GenVertexY, "GenVertexY/F");
    fChain->Branch("GenVertexZ", &GenVertexZ, "GenVertexZ/F");
+
+   //storing refitvertex info	
+   fChain->Branch("RefitVertexX", &RefitVertexX, "RefitVertexX/F");
+   fChain->Branch("RefitVertexY", &RefitVertexY, "RefitVertexY/F");
+   fChain->Branch("RefitVertexZ", &RefitVertexZ, "RefitVertexZ/F");
+   fChain->Branch("matched_pair", &matched_pair, "matched_pair/I");	
 
    fChain->Branch("VxConstitTau1", &VxConstitTau1, "VxConstitTau1/F");
    fChain->Branch("VyConstitTau1", &VyConstitTau1, "VyConstitTau1/F");
