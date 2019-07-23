a1aa5d5d7fbadf853bcef1561f7a626732acfe9b
Mareike Meyer <mareike.meyer@desy.de>
2019-07-23 16:13:14 +0200

    updated 2018 embedding weights

--- NTupleMaker/test/HTauTau_EMu_2018_all_eras/make_configs_2018.sh
+++ NTupleMaker/test/HTauTau_EMu_2018_all_eras/make_configs_2018.sh
@@ -9,3 +9,5 @@ sed -i 's/IsSignal = false/IsSignal = true/g' analysisMacroSynch_em_Signal.conf
 cp analysisMacroSynch_em_Signal.conf analysisMacroSynch_em_Signal_ggh.conf
 sed -i 's/ApplygghReweighting = false/ApplygghReweighting = true/g' analysisMacroSynch_em_Signal_ggh.conf
 sed -i 's/ApplygghUncertainties = false/ApplygghUncertainties = true/g' analysisMacroSynch_em_Signal_ggh.conf
+cp analysisMacroSynch_em_MC.conf analysisMacroSynch_em_Embedded.conf
+sed -i 's/IsEmbedded = false/IsEmbedded = true/g' analysisMacroSynch_em_Embedded.conf
