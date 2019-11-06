1924b681db37d3767aae47aa564fe1066c84fbd2
Mareike Meyer <mareike.meyer@desy.de>
2019-11-06 14:47:56 +0100

    updated location of ST, ttbar and ggh samples, added larger ttbar samples

--- NTupleMaker/test/HTauTau_EMu_2017_all_eras/make_list.sh
+++ NTupleMaker/test/HTauTau_EMu_2017_all_eras/make_list.sh
@@ -2,10 +2,12 @@
 dirData=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/data/MuonEG
 dirDY=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v3
 dirW=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v3
-dirTT=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v3
-dirST=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v3
+dirTT=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v4
+dirTT2=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v3
+dirST=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v4
 dirVV=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v3
 dirSignal=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v3
+dirggh=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v4
 dirEm=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/data/Embedding_emu
 dirWGammaStar=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v3
 dirEWKZ=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v3
@@ -28,8 +30,11 @@ ls $dirW/W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/*root > W3JetsToLNu_TuneC
 ls $dirW/W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/*root > W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8
 
 ls $dirTT/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/*root > TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8
+ls $dirTT2/TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8/*root >> TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8
 ls $dirTT/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/*root > TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8
+ls $dirTT2/TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8/*root >> TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8
 ls $dirTT/TTToHadronic_TuneCP5_13TeV-powheg-pythia8/*root > TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8
+ls $dirTT2/TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8/*root >> TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8
 
 ls $dirST/ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8/*root > ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8
 ls $dirST/ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8/*root > ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8
@@ -40,8 +45,8 @@ ls $dirVV/WW_TuneCP5_13TeV-pythia8/*root > WW_TuneCP5_13TeV-pythia8
 ls $dirVV/WZ_TuneCP5_13TeV-pythia8/*root > WZ_TuneCP5_13TeV-pythia8
 ls $dirVV/ZZ_TuneCP5_13TeV-pythia8/*root > ZZ_TuneCP5_13TeV-pythia8
 
-ls $dirSignal/GluGluHToTauTau_M125_13TeV_powheg_pythia8/*root > GluGluHToTauTau_M125_13TeV_powheg_pythia8
-ls $dirSignal/GluGluHToTauTau_M125_13TeV_powheg_pythia8_ext1/*root >> GluGluHToTauTau_M125_13TeV_powheg_pythia8
+ls $dirggh/GluGluHToTauTau_M125_13TeV_powheg_pythia8_new_pmx/*root > GluGluHToTauTau_M125_13TeV_powheg_pythia8
+ls $dirggh/GluGluHToTauTau_M125_13TeV_powheg_pythia8_ext1/*root >> GluGluHToTauTau_M125_13TeV_powheg_pythia8
 ls $dirSignal/VBFHToTauTau_M125_13TeV_powheg_pythia8/*root > VBFHToTauTau_M125_13TeV_powheg_pythia8
 ls $dirSignal/GluGluHToWWTo2L2Nu_M125_13TeV_powheg2_JHUGenV714_pythia8/*root > GluGluHToWWTo2L2Nu
 ls $dirSignal/VBFHToWWTo2L2Nu_M125_13TeV_powheg2_JHUGenV714_pythia8/*root > VBFHToWWTo2L2Nu_M125_13TeV_powheg2_JHUGenV714_pythia8
