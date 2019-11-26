987ce5af1e210c764a81a27a4e67121097a0374d
Aliaksei Raspiareza <rasp@mail.desy.de>
2019-11-26 10:10:52 +0100

    updated class to apply jet energy scale uncertainties

--- NTupleMaker/interface/JESUncertainties.h
+++ NTupleMaker/interface/JESUncertainties.h
@@ -84,11 +84,64 @@ class JESUncertainties {
     "RelativeStatFSR",
     "RelativeStatHF",
     "RelativeBal",
+    "RelativeSample",
     "SinglePionECAL",
     "SinglePionHCAL",
     "TimePtEta",
     "Total",
   }; // end uncertNames
+
+  // groupped uncertainties
+  std::vector< std::string > groupedUncertNames = {"Eta0To5",
+						   "Eta0To3",
+						   "Eta3To5",
+						   "RelativeBal",
+						   "RelativeSample"};
+
+  int nsrc_Eta0To5 = 13;
+  std::vector<std::string> srcnames_Eta0To5 = {"SinglePionECAL",
+					       "SinglePionHCAL",
+					       "AbsoluteFlavMap",
+					       "AbsoluteMPFBias",
+					       "AbsoluteScale",
+					       "AbsoluteStat",
+					       "Fragmentation",
+					       "FlavorQCD",
+					       "TimePtEta",
+					       "PileUpDataMC",
+					       "RelativeFSR",
+					       "RelativeStatFSR",
+					       "PileUpPtRef"};
+ int nsrc_Eta0To3 = 9;
+  std::vector<std::string> srcnames_Eta0To3 = {"PileUpPtEC1",
+					       "PileUpPtEC2",
+					       "PileUpPtBB",
+					       "RelativeJEREC1",
+					       "RelativeJEREC2",
+					       "RelativePtEC1",
+					       "RelativePtEC2",
+					       "RelativeStatEC",
+					       "RelativePtBB"};
+  int nsrc_Eta3To5 = 4;
+  std::vector<std::string> srcnames_Eta3To5 = {"RelativeStatHF",
+					       "RelativePtHF",
+					       "PileUpPtHF",
+					       "RelativeJERHF"};
+  int nsrc_RelativeBal = 1;
+  std::vector<std::string> srcnames_RelativeBal = {"RelativeBal"};
+  
+  int nsrc_RelativeSample = 1;
+  std::vector<std::string> srcnames_RelativeSample = {"RelativeSample"};
+
+  std::map<std::string, std::vector<std::string> > MapUncert = {
+    {groupedUncertNames[0],srcnames_Eta0To5},
+    {groupedUncertNames[1],srcnames_Eta0To3},
+    {groupedUncertNames[2],srcnames_Eta3To5},
+    {groupedUncertNames[3],srcnames_RelativeBal},
+    {groupedUncertNames[4],srcnames_RelativeSample}
+  };
+
+
 
 };
 
