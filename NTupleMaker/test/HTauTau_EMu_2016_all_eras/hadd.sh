c37d4b19e0284b6ae364ca825eda16f1b46d3476
Mareike Meyer <mareike.meyer@desy.de>
2019-11-29 11:05:47 +0100

    added files for running 2016

---
+++ NTupleMaker/test/HTauTau_EMu_2016_all_eras/hadd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for dir in $(find -maxdepth 1 -type d -name "*_files")
+do
+    filename=${dir:2:${#dir}-8}
+    echo $filename
+	 hadd -f ${filename}.root ${filename}_files/*.root
+done
+
---
+++ NTupleMaker/test/HTauTau_EMu_2016_all_eras/make_parameter_file_2016.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+echo "CONFIGFILE,FILELIST" > parameters.txt
+
+../split_filelist.sh analysisMacroSynch_em_DATA.conf MuonEG_Run2016B 5
+../split_filelist.sh analysisMacroSynch_em_DATA.conf MuonEG_Run2016C 5
+../split_filelist.sh analysisMacroSynch_em_DATA.conf MuonEG_Run2016D 5
+../split_filelist.sh analysisMacroSynch_em_DATA.conf MuonEG_Run2016E 5
+../split_filelist.sh analysisMacroSynch_em_DATA.conf MuonEG_Run2016F 5
+../split_filelist.sh analysisMacroSynch_em_GH.conf MuonEG_Run2016G  5
+../split_filelist.sh analysisMacroSynch_em_GH.conf MuonEG_Run2016H  5
+
+
+#DY Samples
+../split_filelist.sh  analysisMacroSynch_em_DY.conf  DY1JetsToLL_M-50    5
+../split_filelist.sh  analysisMacroSynch_em_DY.conf  DY2JetsToLL_M-50    5
+../split_filelist.sh  analysisMacroSynch_em_DY.conf  DY3JetsToLL_M-50    5
+../split_filelist.sh  analysisMacroSynch_em_DY.conf  DY4JetsToLL_M-50   5
+../split_filelist.sh  analysisMacroSynch_em_DY.conf  DYJetsToLL_M-50   5
+../split_filelist.sh  analysisMacroSynch_em_DY.conf  DYJetsToLL_M-10to50  5
+
+#WJets
+../split_filelist.sh  analysisMacroSynch_em_W.conf  W1JetsToLNu   5
+../split_filelist.sh  analysisMacroSynch_em_W.conf  W2JetsToLNu   5
+../split_filelist.sh  analysisMacroSynch_em_W.conf  W3JetsToLNu   5
+../split_filelist.sh  analysisMacroSynch_em_W.conf  W4JetsToLNu   5
+../split_filelist.sh  analysisMacroSynch_em_W.conf   WJetsToLNu   5
+
+
+#SingleTop 
+../split_filelist.sh  analysisMacroSynch_em_mc.conf  ST_t-channel_antitop  4
+../split_filelist.sh  analysisMacroSynch_em_mc.conf  ST_t-channel_top  4
+../split_filelist.sh  analysisMacroSynch_em_mc.conf  ST_tW_top   4
+../split_filelist.sh  analysisMacroSynch_em_mc.conf  ST_tW_antitop   4
+
+#TTbar
+../split_filelist.sh  analysisMacroSynch_em_mc.conf  TTbar   4
+
+#Diboson 
+../split_filelist.sh  analysisMacroSynch_em_mc.conf  ZZ  1
+../split_filelist.sh  analysisMacroSynch_em_mc.conf  WW  1
+../split_filelist.sh  analysisMacroSynch_em_mc.conf  WZ   1
+
+
+#Signal
+../split_filelist.sh  analysisMacroSynch_em_Signal_ggh.conf  GluGluHToTauTau_M125  4
+../split_filelist.sh analysisMacroSynch_em_Signal_VBF.conf VBFHToTauTau_M125 4
+../split_filelist.sh  analysisMacroSynch_em_Signal_VBF.conf  WminusHToTauTau_M125 1
+../split_filelist.sh  analysisMacroSynch_em_Signal_VBF.conf  WplusHToTauTau_M125 1
+../split_filelist.sh  analysisMacroSynch_em_Signal_VBF.conf  ZHToTauTau_M125 1
+../split_filelist.sh  analysisMacroSynch_em_Signal_VBF.conf  GluGluHToWWTo2L2Nu_M125 1
+../split_filelist.sh  analysisMacroSynch_em_Signal_VBF.conf  VBFHToWWTo2L2Nu_M125 1
+../split_filelist.sh  analysisMacroSynch_em_Signal_VBF.conf  ttHJetToTT_M125 1
+
+#WG and WGstar 
+../split_filelist.sh  analysisMacroSynch_em_WG.conf  WGToLNuG   4
+../split_filelist.sh  analysisMacroSynch_em_W.conf  WGstarToLNuEE   4
+../split_filelist.sh  analysisMacroSynch_em_W.conf   WGstarToLNuMuMu  4
+
+#EWK
+../split_filelist.sh  analysisMacroSynch_em_W.conf EWKWMinus2Jet 5
+../split_filelist.sh  analysisMacroSynch_em_W.conf EWKWPlus2Jets 5
+../split_filelist.sh  analysisMacroSynch_em_DY.conf EWKZ2Jets  5
+
+../split_filelist.sh  analysisMacroSynch_em_Embedded.conf Embedding_Run2016  1
