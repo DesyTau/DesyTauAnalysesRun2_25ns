0f313267c235a2195a371a13782a481bc3a57f71
Mareike Meyer <mareike.meyer@desy.de>
2019-12-04 09:20:35 +0100

    data true for embedded

--- NTupleMaker/test/HTauTau_EMu_2016_all_eras/make_configs_2016.sh
+++ NTupleMaker/test/HTauTau_EMu_2016_all_eras/make_configs_2016.sh
@@ -13,8 +13,9 @@ cp analysisMacroSynch_em_Signal_VBF.conf analysisMacroSynch_em_Signal_ggh.conf
 sed -i 's/ApplygghReweighting = false/ApplygghReweighting = true/g' analysisMacroSynch_em_Signal_ggh.conf
 sed -i 's/ApplygghUncertainties = false/ApplygghUncertainties = true/g' analysisMacroSynch_em_Signal_ggh.conf
 
-cp analysisMacroSynch_em_mc.conf analysisMacroSynch_em_Embedded.conf
+cp analysisMacroSynch_em_DATA.conf analysisMacroSynch_em_Embedded.conf
 sed -i 's/IsEmbedded = false/IsEmbedded = true/g' analysisMacroSynch_em_Embedded.conf
+sed -i 's/IsData = true/IsData = false/g' analysisMacroSynch_em_Embedded.conf
 
 cp analysisMacroSynch_em_W.conf analysisMacroSynch_em_WG.conf
 sed -i 's/RemoveGammaStar = false/RemoveGammaStar = true/g' analysisMacroSynch_em_WG.conf
