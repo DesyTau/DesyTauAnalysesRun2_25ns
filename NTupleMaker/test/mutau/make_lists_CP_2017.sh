ca878a88447285f4b2002f2858762c468bf53ce1
Andrea Cardini <andrea.cardini4@gmail.com>
2020-02-11 18:30:22 +0100

    Updated sample lists for CP analysis

--- NTupleMaker/test/mutau/make_lists_CP_2017.sh
+++ NTupleMaker/test/mutau/make_lists_CP_2017.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-dirDataSingleMuon=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/data/SingleMuon
-dirMC=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v3
-dirMC_v4=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v4
-dirTauSpinner=/pnfs/desy.de/cms/tier2/store/user/acardini/ntuples/2017/mc
+dirDataSingleMuon=/nfs/dust/cms/user/rasp/ntuples/2017/data/SingleMuon
+dirMC=/pnfs/desy.de/cms/tier2/store/user/acardini/ntuples/2017/mc
 dirEmbedded=/pnfs/desy.de/cms/tier2/store/user/acardini/ntuples/2017/embedded/Embedding_mutau
 OUTDIR=./2017
 
@@ -21,46 +19,37 @@ ls $dirMC/DY4JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/*root > $OUTDIR/DY4
 ls $dirMC/DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8/*root > $OUTDIR/DYJetsToLL_M-10to50
 
 ls $dirMC/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/*root > $OUTDIR/WJetsToLNu
-ls $dirMC/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8_ext1/*root >> $OUTDIR/WJetsToLNu
+ls $dirMC/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8_ex1/*root >> $OUTDIR/WJetsToLNu
 ls $dirMC/W1JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/*root > $OUTDIR/W1JetsToLNu
 ls $dirMC/W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/*root > $OUTDIR/W2JetsToLNu
 ls $dirMC/W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/*root > $OUTDIR/W3JetsToLNu
 ls $dirMC/W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/*root > $OUTDIR/W4JetsToLNu
 
-ls $dirMC_v4/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/*root > $OUTDIR/TTTo2L2Nu
-ls $dirMC_v4/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/*root > $OUTDIR/TTToSemiLeptonic
-ls $dirMC_v4/TTToHadronic_TuneCP5_13TeV-powheg-pythia8/*root > $OUTDIR/TTToHadronic
+ls $dirMC/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/*root > $OUTDIR/TTTo2L2Nu
+ls $dirMC/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/*root > $OUTDIR/TTToSemiLeptonic
+ls $dirMC/TTToHadronic_TuneCP5_13TeV-powheg-pythia8/*root > $OUTDIR/TTToHadronic
 ls $dirMC/TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8/*root >> $OUTDIR/TTTo2L2Nu
 ls $dirMC/TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8/*root >> $OUTDIR/TTToSemiLeptonic
 ls $dirMC/TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8/*root >> $OUTDIR/TTToHadronic
 
-ls $dirMC_v4/ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8/*root > $OUTDIR/ST_t-channel_top_4f
-ls $dirMC_v4/ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8/*root > $OUTDIR/ST_t-channel_antitop_4f
-ls $dirMC_v4/ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8/*root > $OUTDIR/ST_tW_top_5f
-ls $dirMC_v4/ST_tW_antitop_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8/*root > $OUTDIR/ST_tW_antitop_5f
+ls $dirMC/ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8/*root > $OUTDIR/ST_t-channel_top_4f
+ls $dirMC/ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8/*root > $OUTDIR/ST_t-channel_antitop_4f
+ls $dirMC/ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8/*root > $OUTDIR/ST_tW_top_5f
+ls $dirMC/ST_tW_antitop_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8/*root > $OUTDIR/ST_tW_antitop_5f
 
 ls $dirMC/WW_TuneCP5_13TeV-pythia8/*root > $OUTDIR/WW
 ls $dirMC/WZ_TuneCP5_13TeV-pythia8/*root > $OUTDIR/WZ
 ls $dirMC/ZZ_TuneCP5_13TeV-pythia8/*root > $OUTDIR/ZZ
 
-ls $dirMC_v4/GluGluHToTauTau_M125_13TeV_powheg_pythia8_ext1/*root > $OUTDIR/GluGluHToTauTau_M125
-ls $dirMC_v4/GluGluHToTauTau_M125_13TeV_powheg_pythia8_new_pmx/*root >> $OUTDIR/GluGluHToTauTau_M125
-ls $dirMC_v4/VBFHToTauTau_M125_13TeV_powheg_pythia8/*root > $OUTDIR/VBFHToTauTau_M125
 
-# # commented since now switching to TauSpinner weights
-# ls $dirMC_v4/SUSYGluGluToHToTauTau_M-130_TuneCUETP8M1_13TeV-pythia8/*root > $OUTDIR/SUSYGluGluToHToTauTau
-# ls $dirMC_v4/SUSYGluGluToHToTauTau_M-120_TuneCUETP8M1_13TeV-pythia8/*root >> $OUTDIR/SUSYGluGluToHToTauTau
-# ls $dirMC_v4/SUSYGluGluToBBHToTauTau_M-130_TuneCUETP8M1_13TeV-pythia8/*root >> $OUTDIR/SUSYGluGluToHToTauTau
-# ls $dirMC_v4/SUSYGluGluToBBHToTauTau_M-120_TuneCUETP8M1_13TeV-pythia8/*root >> $OUTDIR/SUSYGluGluToHToTauTau
+ls $dirDataSingleMuon/SingleMuon_Run2017B/*root > $OUTDIR/SingleMuon_Run2017B
+ls $dirDataSingleMuon/SingleMuon_Run2017C/*root > $OUTDIR/SingleMuon_Run2017C
+ls $dirDataSingleMuon/SingleMuon_Run2017D/*root > $OUTDIR/SingleMuon_Run2017D
+ls $dirDataSingleMuon/SingleMuon_Run2017E/*root > $OUTDIR/SingleMuon_Run2017E
+ls $dirDataSingleMuon/SingleMuon_Run2017F/*root > $OUTDIR/SingleMuon_Run2017F
 
-ls $dirDataSingleMuon/SingleMuon_Run2017B-31Mar2018-v1/*root > $OUTDIR/SingleMuon_Run2017B
-ls $dirDataSingleMuon/SingleMuon_Run2017C-31Mar2018-v1/*root > $OUTDIR/SingleMuon_Run2017C
-ls $dirDataSingleMuon/SingleMuon_Run2017D-31Mar2018-v1/*root > $OUTDIR/SingleMuon_Run2017D
-ls $dirDataSingleMuon/SingleMuon_Run2017E-31Mar2018-v1/*root > $OUTDIR/SingleMuon_Run2017E
-ls $dirDataSingleMuon/SingleMuon_Run2017F-31Mar2018-v1/*root > $OUTDIR/SingleMuon_Run2017F
-
-ls $dirTauSpinner/GluGluHToTauTauUncorrelatedDecay_Filtered_M125/*root > $OUTDIR/GluGluHToTauTauUncorrDecays_M125
-ls $dirTauSpinner/VBFHToTauTauUncorrelatedDecay_Filtered_M125/*root > $OUTDIR/VBFHToTauTauUncorrDecays_M125
+ls $dirMC/GluGluHToTauTauUncorrelatedDecay_Filtered_M125/*root > $OUTDIR/GluGluHToTauTauUncorrDecays_M125
+ls $dirMC/VBFHToTauTauUncorrelatedDecay_Filtered_M125/*root > $OUTDIR/VBFHToTauTauUncorrDecays_M125
 
 # ls /pnfs/desy.de/cms/tier2/store/user/mvandekl/2017/mc/GluGluToHToTauTauNoSpin_Unfiltered_Rev1/*.root > $OUTDIR/GluGluToHToTauTauNoSpinCorr_Unfiltered
 # ls /pnfs/desy.de/cms/tier2/store/user/mvandekl/2017/mc/VBFHToTauTauNoSpin_Unfiltered_Rev1/*.root > $OUTDIR/VBFHToTauTauNoSpinCorr_Unfiltered
