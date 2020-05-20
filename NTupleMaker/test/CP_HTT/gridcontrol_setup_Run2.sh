8112382a47b8603d502241efa0fe978876a9b0f7
Andrea Cardini <andrea.cardini@cern.ch>
2020-05-20 10:36:17 +0200

    Scripts for etau channel

---
+++ NTupleMaker/test/CP_HTT/gridcontrol_setup_Run2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+### the script is to be run with "./gridcontrol_setup_mt_Run2.sh <year={16,17,18}> <channel={mt,et}>"
+
+YEAR=$1
+CHANNEL=$2
+if [[ $CHANNEL == "mt" ]]; then
+    OUTDIR=./mutau/20$YEAR
+else  
+    if [[ $CHANNEL == "et" ]]; then
+	OUTDIR=./etau/20$YEAR
+    else
+	echo "ERROR: please run the script with ./gridcontrol_setup_mt_Run2.sh <year={16,17,18}> <channel={mt,et}>"
+	exit
+    fi
+fi
+
+./make_config_Run2.sh $YEAR MC $CHANNEL
+./make_config_Run2.sh $YEAR data $CHANNEL
+./make_config_Run2.sh $YEAR embedded $CHANNEL
+
+./make_lists_CP_20$YEAR.sh $CHANNEL
+
+cd $OUTDIR
+./make_parameter_file.sh
+cd -
+
+###################################################################################################
+#
+# Change make_parameter_file.sh in the ./20$YEAR directory before running this script
+# After running this script setup the gridcontrol config file (./2017/gc_synch17.conf for example)
+# Change project area, and storage area (se path)
+# Then run grid-control
+#
+###################################################################################################
