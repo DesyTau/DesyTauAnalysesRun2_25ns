ed8199b439c414e8031e3222a450bfa66ffc66b0
Aliaksei Raspiareza <rasp@mail.desy.de>
2021-01-12 12:25:48 +0100

    Updated script to run samples

--- NTupleMaker/test/HToAA/Configs/RunSamples_2018.bash
+++ NTupleMaker/test/HToAA/Configs/RunSamples_2018.bash
@@ -9,13 +9,13 @@ done
 echo submitting jobs for HToAA_AToMuMu_AToTauTau samples
 for i in {4..21}
 do
-    ./HTC_submit_seq.sh analysis_macro analysisMacro_ggH_2018.conf SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-${i} 5
+    ./HTC_submit_seq.sh analysis_macro analysisMacro_ggH_2mu2tau_2018.conf SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-${i} 5
 done
 
 echo submitting jobs for HToAA_To4Tau samples
 for i in {4..21}
 do
-    ./HTC_submit_seq.sh analysis_macro analysisMacro_ggH_2018.conf SUSYGluGluToHToAA_AToTauTau_M-125_M-${i} 5
+    ./HTC_submit_seq.sh analysis_macro analysisMacro_ggH_4tau_2018.conf SUSYGluGluToHToAA_AToTauTau_M-125_M-${i} 5
     ./HTC_submit_seq.sh analysis_macro analysisMacro_VBF_2018.conf SUSYVBFToHToAA_AToTauTau_M-125_M-${i} 5
     ./HTC_submit_seq.sh analysis_macro analysisMacro_VH_2018.conf SUSYVH_HToAA_AToTauTau_M-125_M-${i} 5
     ./HTC_submit_seq.sh analysis_macro analysisMacro_ttH_2018.conf SUSYttH_HToAA_AToTauTau_M-125_M-${i} 5
